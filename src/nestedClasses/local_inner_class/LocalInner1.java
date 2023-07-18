6dcc5967430d48c07c702f489fb937442573ba3f
mirasa2907 <@mirasa2907>
2023-07-18 17:11:44 +0600

    Local inner class

---
+++ src/nestedClasses/local_inner_class/LocalInner1.java
@@ -0,0 +1,53 @@
+package nestedClasses.local_inner_class;
+
+public class LocalInner1 {
+    public static void main(String[] args) {
+        Math math = new Math();
+        math.getResult();
+    }
+}
+
+class Math {
+    private int a = 10;
+    public void getResult() {
+        class Delenie{
+            private int delimoe;
+            private int delitel;
+
+            public int getDelimoe() {
+                return delimoe;
+            }
+
+            public void setDelimoe(int delimoe) {
+                this.delimoe = delimoe;
+            }
+
+            public int getDelitel() {
+                return delitel;
+            }
+
+            public void setDelitel(int delitel) {
+                this.delitel = delitel;
+            }
+
+            public int getChastnoe() {
+                return delimoe/delitel;
+            }
+
+            public int getOstatok() {
+                System.out.println(a);
+                return delimoe % delitel;
+            }
+        }
+
+        Delenie delenie = new Delenie();
+        delenie.setDelimoe(21);
+        delenie.setDelitel(4);
+        System.out.println("Delimoe = " + delenie.getDelimoe());
+        System.out.println("Delitel = " + delenie.getDelitel());
+        System.out.println("Chastnoe = " + delenie.getChastnoe());
+        System.out.println("Ostatok = " + delenie.getOstatok());
+
+
+    }
+}
