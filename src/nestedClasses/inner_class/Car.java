ed48e5edaae3d1b9690626e32596fa2bc68c15b0
mirasa2907 <@mirasa2907>
2023-07-18 16:51:33 +0600

    Inner class

---
+++ src/nestedClasses/inner_class/Car.java
@@ -0,0 +1,55 @@
+package nestedClasses.inner_class;
+
+public class Car {
+    String color;
+    private int doorCount;
+    Engine engine;
+
+    public Car(String color, int doorCount) {
+        this.color = color;
+        this.doorCount = doorCount;
+//        this.engine = this.new Engine(horsePower);
+        Engine e = new Engine(200);
+        System.out.println(e.horsePower);
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "color='" + color + '\'' +
+                ", doorCount=" + doorCount +
+                ", engine=" + engine +
+                '}';
+    }
+
+    public class Engine{
+        private int horsePower;
+
+        public Engine(int horsePower) {
+            this.horsePower = horsePower;
+            System.out.println(doorCount);
+        }
+
+        @Override
+        public String toString() {
+            return "Engine{" +
+                    "horsePower=" + horsePower +
+                    '}';
+        }
+    }
+}
+
+
+class Test{
+    public static void main(String[] args) {
+        Car car = new Car("black", 4);
+        Car.Engine engine = car.new Engine(150);
+        car.setEngine(engine);
+        System.out.println(car);
+    }
+}
+
