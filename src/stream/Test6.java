d256823ce9abb67e3d957a35991d080ea679531b
mirasa2907 <@mirasa2907>
2023-07-21 19:30:49 +0600

    distinct, count, peek

--- src/stream/Test6.java
+++ src/stream/Test6.java
@@ -8,6 +8,12 @@ public class Test6 {
         Stream<Integer> stream2 = Stream.of(1, 2, 3, 4, 5);
         Stream<Integer> stream3 = Stream.of(6, 7, 8, 9, 10);
         Stream<Integer> stream4 = Stream.concat(stream2, stream3);
-        stream4.forEach(System.out::println);
+//        stream4.forEach(System.out::println);
+
+        Stream<Integer> stream5 = Stream.of(1, 2, 3, 4, 5, 1, 2, 3);
+//        stream5.distinct().forEach(System.out::println);
+//        System.out.println(stream5.count());
+
+        System.out.println(stream1.distinct().peek(System.out::println).count());
     }
 }
