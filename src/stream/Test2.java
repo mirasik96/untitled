83d0464daa72dbdd72ee315f76a3919199e9ead3
mirasa2907 <@mirasa2907>
2023-07-21 18:34:47 +0600

    sorted

--- src/stream/Test2.java
+++ src/stream/Test2.java
@@ -19,6 +19,8 @@ public class Test2 {
         students.add(st4);
         students.add(st5);
 
+        students = students.stream().sorted((x,y) -> x.getName().compareTo(y.getName())).collect(Collectors.toList());
+
         students = students.stream().filter(element -> element.getAge() > 22 && element.getAvgGrade() < 7.2).collect(Collectors.toList());
         System.out.println(students);
 
