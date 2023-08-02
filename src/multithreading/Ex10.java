69757b2545c8035d7577273ff0d3e2d310aaec02
mirasa2907 <@mirasa2907>
2023-08-02 16:08:11 +0600

    Data race, Synchronized methods

---
+++ src/multithreading/Ex10.java
@@ -0,0 +1,33 @@
+package multithreading;
+
+public class Ex10 {
+    public static void main(String[] args) {
+        MyRunnableImpl1 runnableImpl1 = new MyRunnableImpl1();
+        Thread thread1 = new Thread(runnableImpl1);
+        Thread thread2 = new Thread(runnableImpl1);
+        Thread thread3 = new Thread(runnableImpl1);
+        thread1.start();
+        thread2.start();
+        thread3.start();
+    }
+
+}
+
+class Counter {
+    volatile static int count = 0;
+}
+
+class MyRunnableImpl1 implements Runnable {
+
+    public synchronized void increment() {
+        Counter.count++;
+        System.out.print(Counter.count + " ");
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 3; i++) {
+            increment();
+        }
+    }
+}
