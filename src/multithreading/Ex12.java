38e26b38b67a0e2cf417e322321bb33a590c4b22
mirasa2907 <@mirasa2907>
2023-08-02 16:55:57 +0600

    Понятие "монитор", Synchronized blocks

---
+++ src/multithreading/Ex12.java
@@ -0,0 +1,82 @@
+package multithreading;
+
+import java.util.Objects;
+
+public class Ex12 {
+    static final Object lock = new Object();
+
+    void mobileCall() {
+        synchronized (lock) {
+            System.out.println(this);
+            System.out.println("Mobile call starts");
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println("Mobile call ends");
+        }
+    }
+
+    void skypeCall() {
+        synchronized (lock) {
+            System.out.println(this);
+            System.out.println("Skype call starts");
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println("Skype call ends");
+        }
+    }
+
+
+    void whatsappCall() {
+        synchronized (lock) {
+            System.out.println(this);
+            System.out.println("Whatsapp call starts");
+            try {
+                Thread.sleep(7000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println("Whatsapp call ends");
+        }
+    }
+
+    public static void main(String[] args) {
+        Thread thread1 = new Thread(new RunnableImplMobile());
+        Thread thread2 = new Thread(new RunnableImplSkype());
+        Thread thread3 = new Thread(new RunnableImplWhatsapp());
+
+        thread1.start();
+        thread2.start();
+        thread3.start();
+    }
+}
+
+class RunnableImplMobile implements Runnable {
+    @Override
+    public void run() {
+        new Ex12().mobileCall();
+    }
+}
+
+class RunnableImplSkype implements Runnable {
+    @Override
+    public void run() {
+        new Ex12().skypeCall();
+    }
+}
+
+class RunnableImplWhatsapp implements Runnable {
+    @Override
+    public void run() {
+        new Ex12().whatsappCall();
+    }
+}
+
+class Car {
+
+}
